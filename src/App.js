1d7ae9c34999b9fd0bc23276dc9359fc0b88dbc2
Tom <demart.tom@gmail.com>
2022-11-27 15:58:20 -0700

    Adding highlights page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Fun from "./pages/Fun";
+import Highlights from "./pages/Highlights";
 import Error404 from "./pages/Error404";
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/fun" element={<Fun />} />
+          <Route path="/highlights" element={<Highlights />} />
           <Route path="*" element={<Error404 />} />
         </Routes>
       </Router>
---
+++ src/pages/Highlights.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export default function Highlights() {
+  return (
+    <>
+      <Container maxWidth="xl">
+        <Typography variant="h3">Highlights</Typography>
+      </Container>
+    </>
+  );
+}
